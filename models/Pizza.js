0910c5144504730f519c3edc9ced6df25bd9648f
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-02-25 12:02:02 -0600

    Create and delete Comments

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
   pizzaName: {
@@ -9,13 +10,34 @@ const PizzaSchema = new Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
   },
   size: {
     type: String,
     default: 'Large'
   },
-  toppings: []
+  toppings: [],
+  comments: [
+    {
+      type: String,
+      type: Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ]
+},
+{
+  toJSON: {
+    virtuals: true,
+    getters: true
+  },
+  id: false
+},
+
+);
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
 });
 
 const Pizza = model('Pizza', PizzaSchema);
--- public/assets/js/pizza-list.js
+++ public/assets/js/pizza-list.js
@@ -23,6 +23,18 @@ const printPizza = ({ _id, pizzaName, toppings, size, commentCount, createdBy, c
       </div>
     </div>
   `;
+  
 
   $pizzaList.innerHTML += pizzaCard;
 };
+const getPizzaList = () => {
+  fetch('/api/pizzas')
+    .then(response => response.json())
+    .then(pizzaListArr => {
+      pizzaListArr.forEach(printPizza);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+getPizzaList();
