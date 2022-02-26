2f4cce85ffae839b0998f7886b237d797204cf52
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-02-26 05:52:05 -0600

    Comment model

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -13,31 +13,24 @@ const pizzaController = {
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
-        res.status(400).json(err);
+        res.sendStatus(400);
       });
   },
 
   // get one pizza by id
- // get one pizza by id
-getPizzaById({ params }, res) {
-  Pizza.findOne({ _id: params.id })
-    .populate({
-      path: 'comments',
-      select: '-__v'
-    })
-    .select('-__v')
-    .then(dbPizzaData => {
-      if (!dbPizzaData) {
-        res.status(404).json({ message: 'No pizza found with this id!' });
-        return;
-      }
-      res.json(dbPizzaData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-},
+  getPizzaById({ params }, res) {
+    Pizza.findOne({ _id: params.id })
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
 
   // createPizza
   createPizza({ body }, res) {
@@ -56,20 +49,14 @@ getPizzaById({ params }, res) {
         }
         res.json(dbPizzaData);
       })
-      .catch(err => res.status(400).json(err));
+      .catch(err => res.json(err));
   },
 
   // delete pizza
   deletePizza({ params }, res) {
     Pizza.findOneAndDelete({ _id: params.id })
-      .then(dbPizzaData => {
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
-      .catch(err => res.status(400).json(err));
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
   }
 };
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
-const pizzaRoutes = require('./pizza-routes');
-
 const commentRoutes = require('./comment-routes');
-
+const pizzaRoutes = require('./pizza-routes');
 
 router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
-
 
 module.exports = router;
